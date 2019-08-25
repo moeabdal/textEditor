77f100942a400942ad81e5a87067a320710342a8
moeabdal <mohammed.h.abdal@gmail.com>
2019-08-25 22:33:16 +0300

    Finished task

--- src/App.js
+++ src/App.js
@@ -8,13 +8,28 @@ const styles = {
 };
 
 class App extends Component {
+  state = {
+    color: "black",
+    bold: false,
+    italic: false,
+    underline: false
+  };
+
+  selectColor = color => {
+    this.setState({ color: color });
+  };
   render() {
     let styleNames = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
     let stylingBoxes = styleNames.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          onClick={() => this.setState({ [style]: !this.state[style] })}
+          className={`btn btn-${this.state[style] ? "primary" : "light"}`}
+          style={styles[style]}
+          key={style}
+        >
           {style}
         </button>
       );
@@ -25,6 +40,7 @@ class App extends Component {
         <button
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
+          onClick={() => this.selectColor(color)}
         />
       );
     });
@@ -32,7 +48,14 @@ class App extends Component {
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea
+          style={{
+            color: this.state.color,
+            fontWeight: this.state.bold ? "bold" : "",
+            fontStyle: this.state.italic ? "italic" : "",
+            textDecorationLine: this.state.underline ? "underline" : ""
+          }}
+        />
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
